ef9d3bdf6df6e82aed4322f1b46d0a300a234a9d
Luashine <103937213+Luashine@users.noreply.github.com>
2023-12-13 00:10:03 +0100

    Use argparse library for argv parsing

--- convert-triggerdata.lua
+++ convert-triggerdata.lua
@@ -179,23 +179,32 @@ function stderr(...)
 end
 
 function main(args)
-	local outputTypes = {none = 0, json = 1, lua = 2}
-	local outputType = outputTypes.json
-	local filePaths = {}
+	local argparse = require"lib.argparse@27967d7.argparse"
+	local parser = argparse()
+		:name("convert-triggerdata.lua")
+		:description("A tool to convert Warcraft 3's code definitions for GUI Trigger Editor from triggerdata.txt")
+		:epilog("Source code: https://github.com/Luashine/convert-triggerdata")
+		:add_complete()
+		:add_help_command()
+		:require_command(false)
 
-	for k, v in pairs(args) do
-		if not v:match("^%-%-") then
-			table.insert(filePaths, v)
-		elseif v == "--json" then
-			outputType = outputTypes.json
-		elseif v == "--lua" then
-			outputType = outputTypes.lua
-		elseif v == "--none" then
-			outputType = outputTypes.none
-		elseif v:find("^%-%-") then
-			error("Unknown command-line option: '".. tostring(v) .."'")
-		end
-	end
+	parser:group("Input data",
+		parser:argument("triggerdata", "Trigger definition file")
+			:args("1+"):argname("<triggerdata.txt>"),
+
+		parser:option("-t --translate", "WE localization file, first file has highest priority")
+			:args("*"):argname("<strings.txt>"):action("concat"):hidden(true)
+	)
+
+	parser:group("Output format",
+		parser:option("-f --format", "output format")
+			:args(1)
+			:default("none")
+			:choices({"json", "lua", "none"})
+	)
+
+	local pargs = parser:parse(args)
+	local filePaths = pargs.triggerdata
 
 	local data = {}
 
@@ -207,16 +216,13 @@ function main(args)
 		file:close()
 	end
 
-	if outputType == outputTypes.json then
+	if pargs.format == "json" then
 		local JSON = require"lib.JSON"
 		io.stdout:write(JSON:encode_pretty(data), "\n")
-	end
 
-	if outputType == outputTypes.lua then
+	elseif pargs.format == "lua" then
 		local serpent = require("lib.serpent@139fc18.src.serpent")
 		io.stdout:write(serpent.block(data, {comment = false}), "\n")
-
-
 	end
 end
 
